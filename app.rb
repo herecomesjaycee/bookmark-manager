aa4ab19a0014920c281b15632b8d86fe45e21716
Justinio14 <jbutwell@hotmail.com>
2017-01-24 13:30:57 +0000

    ex10 completed

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
-
+require './models/link'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    erb :index
 
+  get '/' do
+    redirect '/links'
+  end
+
+  get '/links' do
+    @links = Link.all # try to access a property
+
+    erb :links
   end
 
   # start the server if ruby file executed directly
